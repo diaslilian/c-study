2cdce585abee4a133475c2ae4dbb1a14fccfe194
diaslilian <lilian.dias25@gmail.com>
2022-05-06 19:25:56 -0300

    add data type tutorial

---
+++ tutoriais/data-types.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  // int = para numeros inteiros
+  int age = 40;
+
+  // double = numeros decimais
+  double gpa = 3.6;
+
+  // char com aspas simples '' = só recebe um caracter
+  char grade = 'A';
+
+  // char array[] e com aspas duplas "" = recebe uma string, uma coleção de caracteres
+  char phrase[] = "String of characters";
+
+  // pausar aplicação
+  system("pause");
+
+  return 0;
+}
