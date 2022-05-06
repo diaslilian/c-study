784c9165193c12da36cfb0f646976c2da8f592d8
diaslilian <lilian.dias25@gmail.com>
2022-05-06 19:25:08 -0300

    tutorial variables

--- tutoriais/variables.c
+++ tutoriais/variables.c
@@ -3,10 +3,13 @@
 
 int main()
 {
-  printf("There once was a man named George\n");
-  printf("he was 70 years old.\n");
-  printf("He really liked the name George\n");
-  printf("but did not like being 70.\n");
+  char characterName[] = "John";
+  int characterAge = 35;
+
+  printf("There once was a man named %s\n", characterName);
+  printf("he was %d years old.\n", characterAge);
+  printf("He really liked the name %s\n", characterName);
+  printf("but did not like being %d.\n", characterAge);
 
   return 0;
 }
