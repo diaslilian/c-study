23f009c98d18f935d0436e32e122c2d55c527669
diaslilian <lilian.dias25@gmail.com>
2022-05-30 17:42:44 -0300

    update data types

--- tutoriais/data-types.c
+++ tutoriais/data-types.c
@@ -6,6 +6,10 @@ int main()
   // int = para numeros inteiros
   int age = 40;
 
+  // Os tipos de dados float e double são usados para guardar números reais. A diferença entre eles é a precisão. O float tem uma precisão de 6 casas decimais e o double tem uma precisão de 10 casas decimais.
+
+  float num = 7.2;
+
   // double = numeros decimais
   double gpa = 3.6;
 
